dcb12f4efd814feef251ec63b14fe608ef308a76
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-06-03 23:27:04 +0200

    Fix errors

--- rental.rb
+++ rental.rb
@@ -1,11 +1,14 @@
 class Rental
+  attr_reader :person, :book
   attr_accessor :date
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
-    @book = book
-    book.rentals << self
+
     @person = person
     person.rentals << self
+
+    @book = book
+    book.rentals << self
   end
 end
